98b9473159d3e656dd2d33d47f5b5b5a35be2fe8
HelloUser <beta.webdev@gmail.com>
2012-05-25 10:05:08 +0100

    Premier commit

---
+++ wdCalendar/src/Plugins/datepicker_lang_FR.js
@@ -0,0 +1,37 @@
+﻿var i18n = $.extend({}, i18n || {}, {
+    datepicker: {
+        dateformat: {
+            "fulldayvalue": "dd/MM/yyyy",
+            "separator": "/",
+            "year_index": 2,
+            "month_index": 1,
+            "day_index": 0,
+            "sun": "Dim",
+            "mon": "Lun",
+            "tue": "Mar",
+            "wed": "Mer",
+            "thu": "Jeu",
+            "fri": "Ven",
+            "sat": "Sam",
+            "jan": "Jan",
+            "feb": "Fév",
+            "mar": "Mar",
+            "apr": "Avr",
+            "may": "Mai",
+            "jun": "Jun",
+            "jul": "Jul",
+            "aug": "Aoû",
+            "sep": "Sep",
+            "oct": "Oct",
+            "nov": "Nov",
+            "dec": "Déc",
+            "postfix": ""
+        },
+        ok: " Ok ",
+        cancel: "Annuler",
+        today: "Aujourdhuit",
+        prev_month_title: "mois précédent",
+        next_month_title: "mois suivant"
+    }
+});
+
---
+++ wdCalendar/test.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head id="Head1">
+    <title>	My Calendar </title>
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    <link href="css/dailog.css" rel="stylesheet" type="text/css" />
+    <link href="css/calendar.css" rel="stylesheet" type="text/css" /> 
+    <link href="css/dp.css" rel="stylesheet" type="text/css" />   
+    <link href="css/alert.css" rel="stylesheet" type="text/css" /> 
+    <link href="css/main.css" rel="stylesheet" type="text/css" /> 
+    
+
+    <script src="src/jquery.js" type="text/javascript"></script>  
+    
+    <script src="src/Plugins/Common.js" type="text/javascript"></script>    
+    <script src="src/Plugins/datepicker_lang_FR.js" type="text/javascript"></script>     
+    <script src="src/Plugins/jquery.datepicker.js" type="text/javascript"></script>
+
+    <script src="src/Plugins/jquery.alert.js" type="text/javascript"></script>    
+    <script src="src/Plugins/jquery.ifrmdailog.js" defer="defer" type="text/javascript"></script>
+    <script src="src/Plugins/wdCalendar_lang_FR.js" type="text/javascript"></script>    
+    <script src="src/Plugins/jquery.calendar.js" type="text/javascript"></script>   
+    
+    
+</head>
+<body>
+   
+    <iframe frameborder="0" style="border:none;width:600px;height:400px" src="http://localhost/astuces/agenda/wdCalendar/wdCalendar/sample.php" border="0" id="dailog_iframe_1337616198820"></iframe>
+</body>
+</html>
